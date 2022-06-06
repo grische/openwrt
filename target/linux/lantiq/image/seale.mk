fd541ebacd970ca1d9df64361e1afb8d9f68195d
Grische <github@grische.xyz>
2022-06-06 22:20:52 +0200

    Fix 7560 packages

--- target/linux/lantiq/image/seale.mk
+++ target/linux/lantiq/image/seale.mk
@@ -5,7 +5,6 @@ define Device/avm_fritz7560
   DEVICE_MODEL := FRITZ!Box 7560
   KERNEL_SIZE := 4096k
   IMAGE_SIZE := 49152k
-  DEVICE_PACKAGES := kmod-usb3 fritz-tffs kmod-avm-wasp kmod-avm-wasp \
-	-kmod-owl-loader
+  DEVICE_PACKAGES := kmod-ath9k kmod-usb3 kmod-owl-loader wpad-basic-wolfssl fritz-tffs-nand fritz-caldata
 endef
 TARGET_DEVICES += avm_fritz7560
