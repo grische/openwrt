f33540497912c9bc596b7a39bbaf6f674d515b11
Grische <github@grische.xyz>
2022-06-09 08:49:21 +0200

    Reverting target/linux/lantiq/image/seale.mk

--- target/linux/lantiq/image/seale.mk
+++ target/linux/lantiq/image/seale.mk
@@ -1,10 +1,10 @@
 define Device/avm_fritz7560
+  $(Device/dsa-migration)
   $(Device/AVM)
   $(Device/NAND)
   DEVICE_MODEL := FRITZ!Box 7560
   KERNEL_SIZE := 4096k
   IMAGE_SIZE := 49152k
-  # DEVICE_PACKAGES := kmod-usb3 kmod-owl-loader wpad-basic-wolfssl fritz-tffs-nand fritz-caldata
-  DEVICE_PACKAGES := fritz-tffs-nand fritz-caldata
+  DEVICE_PACKAGES := kmod-ath9k kmod-usb3 kmod-owl-loader wpad-basic-wolfssl fritz-tffs-nand fritz-caldata
 endef
 TARGET_DEVICES += avm_fritz7560
